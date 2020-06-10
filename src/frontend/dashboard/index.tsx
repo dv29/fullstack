417e64cac6d2038e496ced5674f342913659f38b
Vora, Deep <deepvora92@gmail.com>
2020-06-10 18:03:42 -0500

    testing multiple package;

--- src/frontend/dashboard/index.tsx
+++ src/frontend/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as styles from "./styles.css";
+import { name } from "./name";
 
 ReactDOM.render(
-  <h1 className={styles.h1}>Hello, world!</h1>,
+  <h1 className={styles.h1}>Hello, {name()}</h1>,
   document.getElementById("root")
 );
