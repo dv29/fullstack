44320c725933085022ca4a9534415b1f862865a7
Vora, Deep <deepvora92@gmail.com>
2020-06-25 10:51:17 -0500

    docker updates;

--- src/infrastructure/envoy/docker-entrypoint.sh
+++ src/infrastructure/envoy/docker-entrypoint.sh
@@ -6,4 +6,5 @@ cat /etc/envoy/envoy.yaml.tmpl | envsubst \$ENVOY_ADMIN_PORT,\$ENVOY_GRPC_PORT,\
 
 echo "Starting Envoy..."
 # /usr/local/bin/envoy -c /etc/envoy/envoy.yaml -l trace --log-path /tmp/envoy_info.log
-/usr/local/bin/envoy -c /etc/envoy/envoy.yaml -l trace
+# log options -> [trace][debug][info][warning|warn][error][critical][off]
+/usr/local/bin/envoy -c /etc/envoy/envoy.yaml -l info
